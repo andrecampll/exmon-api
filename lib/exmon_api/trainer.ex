acf13b052210de3919fcc298d1032f2db42ec741
André <andrevictor50@gmail.com>
2020-12-08 17:03:42 -0300

    added first trainer schema and added changeset

---
+++ lib/exmon_api/trainer.ex
@@ -0,0 +1,19 @@
+defmodule ExmonApi.Trainer do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  @required_params [:name, :password_hash]
+
+  schema "trainers" do
+    field :name, :string
+    field :password_hash, :string
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+  end
+end
