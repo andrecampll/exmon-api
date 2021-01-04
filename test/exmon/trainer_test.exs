8cec051157589793a0b7e8e7593c88e355813dda
André <andrevictor50@gmail.com>
2021-01-04 15:49:20 -0300

    testing trainer build

--- test/exmon/trainer_test.exs
+++ test/exmon/trainer_test.exs
@@ -37,4 +37,24 @@ defmodule ExmonApi.TrainerTest do
       assert errors_on(response) == %{name: ["can't be blank"]}
     end
   end
+
+  describe "build/1" do
+    test "should be able to return a valid Trainer struct" do
+      params = %{name: "André", password: "123123"}
+
+      response = Trainer.build(params)
+
+      assert {:ok, %Trainer{name: "André", password: "123123"}} = response
+    end
+
+    test "should not be able to return a valid Trainer struct" do
+      params = %{password: "123123"}
+
+      {:error, response} = Trainer.build(params)
+
+      assert %Ecto.Changeset{ valid?: false } = response
+
+      assert errors_on(response) == %{name: ["can't be blank"]}
+    end
+  end
 end
