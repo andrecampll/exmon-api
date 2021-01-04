6e2661e6e3e0ebe5e8c6b2823229d9597a0b8647
André <andrevictor50@gmail.com>
2021-01-04 18:08:57 -0300

    added success pokemon build test

--- test/exmon/trainer_pokemon_test.exs
+++ test/exmon/trainer_pokemon_test.exs
@@ -47,4 +47,32 @@ defmodule ExmonApi.TrainerPokemonTest do
       assert errors_on(response) == %{nickname: ["can't be blank"]}
     end
   end
+
+  describe "build/1" do
+    test "should be able to return a valid struct" do
+      trainer_id = UUID.generate()
+
+      params = %{
+        nickname: "Fofo",
+        weight: 80,
+        types: ["Normal"],
+        name: "furret",
+        trainer_id: trainer_id,
+      }
+
+      response = Pokemon.build(params)
+
+      assert {
+        :ok,
+        %Pokemon{
+          inserted_at: nil,
+          name: "furret",
+          nickname: "Fofo",
+          trainer_id: trainer_id,
+          types: ["Normal"],
+          weight: 80.0
+        },
+      } = response
+    end
+  end
 end
