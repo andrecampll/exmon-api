d17848f18a2a52d236890ede3b14198140bcecea
André <andrevictor50@gmail.com>
2021-01-07 12:09:16 -0300

    authenticating user by password

--- lib/exmon_api_web/auth/guardian.ex
+++ lib/exmon_api_web/auth/guardian.ex
@@ -1,6 +1,8 @@
 defmodule ExmonApiWeb.Auth.Guardian do
   use Guardian, otp_app: :exmon_api
 
+  alias ExmonApi.{Repo, Trainer}
+
   def subject_for_token(trainer, _claims) do
     sub = to_string(trainer.id)
     {:ok, sub}
@@ -10,5 +12,32 @@ defmodule ExmonApiWeb.Auth.Guardian do
     claims
     |> Map.get("sub")
     |> ExmonApi.read_trainer()
+  end
+
+  def authenticate(%{
+    "id" => trainer_id,
+    "password" => password
+  }) do
+    case Repo.get(Trainer, trainer_id) do
+      nil -> {:error, "Trainer not found."}
+      trainer -> validate_password(trainer, password)
+    end
+  end
+
+  def validate_password(
+    %Trainer{
+      password_hash: hash
+    } = trainer,
+    password
+  ) do
+    case Argon2.verify_pass(password, hash) do
+      true -> create_token(trainer)
+      false -> {:error, :unauthorized}
+    end
+  end
+
+  def create_token(trainer) do
+    {:ok, token, _claims} = encode_and_sign(trainer)
+    {:ok, token}
   end
 end
